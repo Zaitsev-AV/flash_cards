efa20c65583888988da3128180d1da76b26f47b5
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-06-25 14:08:14 +0300

    added styles for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      Hello
+      <Button variant={'link'} as={'a'} href={'abrakadabra.com'}>
+        LINK
+      </Button>
+    </div>
+  )
 }
