0d5436a1ee358d0441e235b3c018ebda4de5a984
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-07-03 20:00:16 +0300

    add radio-group

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button'
 import { CustomCheckbox } from '@/components/ui/checkbox'
 import { AvatarDropdownMenu } from '@/components/ui/dropdown-menu'
 import { Header } from '@/components/ui/header'
+import { CustomRadioGroup } from '@/components/ui/radio-group/custom-radio-group.tsx'
 import { CustomSelect } from '@/components/ui/select'
 import { CustomSlider } from '@/components/ui/slider'
 import { CustomTabs } from '@/components/ui/switch'
@@ -15,6 +16,23 @@ import { Typography } from '@/components/ui/typography'
 
 export function App() {
   const [checked, setChecked] = useState(false)
+  const radioItems = [
+    {
+      label: 'Radio_1',
+      id: '1231235dww',
+      value: 'value_1',
+    },
+    {
+      label: 'Radio_2',
+      id: '5851616dww',
+      value: 'value_2',
+    },
+    {
+      label: 'Radio_3',
+      id: '885222sdfs',
+      value: 'value_3',
+    },
+  ]
 
   return (
     <div>
@@ -48,11 +66,14 @@ export function App() {
         <CustomSlider />
       </div>
       <div style={{ paddingLeft: '50%', paddingBottom: '250px', width: '500px', height: '500px' }}>
-        <CustomSelect items={['item_1', 'item_2', 'item_3', 'item_4', 'item_5']} />
+        <CustomSelect items={['item_1', 'item_2', 'item_3', 'item_4', 'item_5']} label={'label'} />
       </div>
       <LoginForm />
       <div style={{ paddingLeft: '40%', paddingBottom: '250px', paddingTop: '250px' }}>
         <CustomTabs tabs={['My Packs', 'All Packs']} disabled={true} />
+      </div>
+      <div style={{ paddingLeft: '40%', paddingBottom: '250px', paddingTop: '250px' }}>
+        <CustomRadioGroup items={radioItems} onChange={() => {}} />
       </div>
     </div>
   )
